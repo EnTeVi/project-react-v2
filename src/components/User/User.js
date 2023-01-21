5c8ca823e08cfef58e9da88d76a3914a546aae35
EnTeVi <andriy.lytvynchuk@gmail.com>
2023-01-21 22:52:21 +0200

    style

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,24 +1,33 @@
 import './User.css';
 
-const User = ({user, liftUser}) => {
+const User = ({user, liftUser, superLift}) => {
     let {id, name, email, phone, website} = user;
+
+
 
     return (
         <div className='box-us'>
-            <p>Id: {id}</p>
-            <p>Name: {name}</p>
+            <div>
+                <details className='details'>
+                    <summary><h3>Details</h3><p>Id: {id}</p><p>Name: {name}</p></summary>
+                    <p>Email: {email}</p>
+                    <p>Phone: {phone}</p>
+                    <p>Website: {website}</p>
+                </details>
 
-            <button onClick={() => {
-                liftUser(
-                    <div>
-                        <p>Id: {id}</p>
-                        <p>Name: {name}</p>
-                        <p>Email: {email}</p>
-                        <p>Phone: {phone}</p>
-                        <p>Website: {website}</p>
-                    </div>
-                )
-            }}>Details</button>
+                {/*{  superLift ()  }*/}
+            </div>
+
+            {/*<button onClick={() => {*/}
+            {/*    liftUser(*/}
+            {/*        <div>*/}
+            {/*            <p>Email: {email}</p>*/}
+            {/*            <p>Phone: {phone}</p>*/}
+            {/*            <p>Website: {website}</p>*/}
+            {/*        </div>*/}
+            {/*    )*/}
+            {/*}}>Details*/}
+            {/*</button>*/}
         </div>
     );
 };
