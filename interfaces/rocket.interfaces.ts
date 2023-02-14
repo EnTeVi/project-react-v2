12411b7a7016771ad548f81bb405ab32f2cfd4bd
EnTeVi <andriy.lytvynchuk@gmail.com>
2023-02-14 13:33:08 +0200

    ts

---
+++ interfaces/rocket.interfaces.ts
@@ -0,0 +1,42 @@
+interface IRocket {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: {
+        site_name_long: string
+    },
+    links: {
+        article_link: null,
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: [
+                ICores
+            ]
+        },
+        second_stage: {
+            payloads: [
+                IPayloads
+            ]
+        }
+    }
+}
+
+interface ICores {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: string | boolean
+    }
+}
+
+interface IPayloads {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+
+export {
+    IRocket
+}
